b4073c882fe41521a3ca7edaf8432b8874ecace1
Jens W. Klein <jens@bluedynamics.com>
2014-02-09 17:44:46 +0100

    try to get a classic in new InteractiveShellEmbed

--- src/interlude/__init__.py
+++ src/interlude/__init__.py
@@ -15,7 +15,13 @@ try:
 except ImportError:
     HAS_IPYTHON = False
 
-def interact(locals=None, use_ipython=True, ipython_argv=['-classic']):
+BANNER = """
+Interlude DocTest Interactive Console - (c) BlueDynamics Alliance
+Note: You have the same locals available as in your test-case.
+Ctrl-D ends session and continues testing.
+"""
+
+def interact(locals=None, use_ipython=True, doctest_prompt=True):
     """Provides an interactive shell aka console inside your testcase.
 
     It looks exact like in a doctestcase and you can copy and paste
@@ -27,16 +33,18 @@ def interact(locals=None, use_ipython=True, ipython_argv=['-classic']):
 
         >>> interact( locals() ) #doctest: +SKIP
 
-    locals -- passed to InteractiveInterpreter.__init__()
+    locals
+        locals available in shell
+
+    use_ipython
+        enabled/disable ipython (only if module installed)
+
+    doctest_prompt
+        use doctest style prompt in ipython (only if module installed)
     """
     savestdout = sys.stdout
     sys.stdout = sys.stderr
     sys.stderr.write('\n' + '=' * 78)
-    sys.stdout.write("""
-Interlude DocTest Interactive Console - (c) BlueDynamics Alliance
-Note: You have the same locals available as in your test-case.
-Ctrl-D ends session and continues testing.
-""")
 
     if use_ipython and HAS_IPYTHON:
         # from IPython.lib.deepreload import reload as dreload
@@ -46,13 +54,24 @@ Ctrl-D ends session and continues testing.
             ipshell = IPShellEmbed(user_ns=locals)
             ipshell(argv=ipython_argv)
         except ImportError:
-            from IPython.frontend.terminal.embed import InteractiveShellEmbed
-            ipshell = InteractiveShellEmbed(user_ns=locals)
-            ipshell(argv=ipython_argv)
+            try:
+                from IPython.terminal.embed import InteractiveShellEmbed
+            except ImportError:
+                # not sure if this is needed for bbb
+                from IPython.terminal.frontend.embed import InteractiveShellEmbed
+            from IPython.config.loader import Config
+            cfg = Config()
+            shell_config = cfg.InteractiveShellEmbed
+            shell_config.prompt_in1 = '    >>> '
+            shell_config.prompt_in2 = '    ... '
+            shell_config.prompt_out = '    '
+            ipshell = InteractiveShellEmbed(user_ns=locals, config=cfg)
+            ipshell(BANNER)
     else:
+        sys.stdout.write(BANNER)
         console = code.InteractiveConsole(locals)
         console.interact()
 
-    sys.stdout.write('\nend of Interlude DocTest Interactive Console session\n')
+    sys.stdout.write('\nEnd of Interlude DocTest Interactive Console session\n')
     sys.stdout.write('=' * 78 + '\n')
     sys.stdout = savestdout
